4dca10af0bee420ed43509498554ea2fdf2f8974
ALPHAtwentyone <114674087+ALPHAtwentyone@users.noreply.github.com>
2022-11-22 10:40:57 +0100

    PROJECt6

--- inline2/regix.js
+++ inline2/regix.js
@@ -1,3 +1,122 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('firstname').onkeyup=function(){
+    var firstname = document.getElementById('firstname').value;
+    var regex = /^[a-z]{1,30}$/ig;
+    if(firstname === '') {
+        document.getElementById('name').innerHTML = 'Nome is required.';
+        document.getElementById('firstname').style.borderColor="red";
+
+    } else if (regex.test(firstname)) {
+        document.getElementById('name').innerHTML = '';
+        document.getElementById('firstname').style.borderColor="#09c372";
+
+       
+    } else if (firstname.length>30){
+        document.getElementById('name').innerHTML = 'where are u goin';
+        
+    } else {
+        document.getElementById('name').innerHTML = 'dont use numers or chifre';
+        document.getElementById('firstname').style.borderColor="red";
+
+       
+    }
+
+    
+}
+document.getElementById('lastname').onkeyup=function(){
+//lastname
+var lastname = document.getElementById('lastname').value;
+var regex = /^[a-z]{1,30}$/ig;
+if(lastname === '') {
+    document.getElementById('namelast').innerHTML = 'Nome is required.';
+    document.getElementById('lastname').style.borderColor="red";
+
+} else if (regex.test(lastname)) {
+    document.getElementById('namelast').innerHTML = '';
+    document.getElementById('lastname').style.borderColor="#09c372";
+
+   
+} else if (lastname.length>30){
+    document.getElementById('namelast').innerHTML = 'where are u goin';
+    
+} else {
+    document.getElementById('namelast').innerHTML = 'dont use numers or chifre';
+    document.getElementById('lastname').style.borderColor="red";
+
+   
+}
+}
+
+
+document.getElementById('email').onkeyup=function(){
+    
+    //email
+    var email = document.getElementById('email').value;
+    var regex = /^[a-z]+\.[a-z]+\@ofppt.ma$/ig;
+    if(email === '') {
+        document.getElementById('paraemail').innerHTML = 'Email is required.';
+        document.getElementById('email').style.borderColor="red";
+
+    } else if (regex.test(email)) {
+        document.getElementById('paraemail').innerHTML = '';
+        document.getElementById('email').style.borderColor="#09c372";
+
+    } else {
+        document.getElementById('paraemail').innerHTML = 'Email Non Valid.';
+        document.getElementById('email').style.borderColor="red";
+         
+     
+    }
+
+}
+document.getElementById('telephone').onkeyup=function(){
+    
+    // telephone 
+    var telephone = document.getElementById('telephone').value;
+    var regex = /^[0]+[5-7]+\d{8}$/ig;
+    if(telephone === '') {
+        document.getElementById('paratele').innerHTML = 'Telephone is required.';
+        document.getElementById('telephone').style.borderColor="red";
+
+    } else if (regex.test(telephone)) {
+        document.getElementById('paratele').innerHTML = '';
+        document.getElementById('telephone').style.borderColor="#09c372";
+
+        
+    } else if (telephone.length>10){
+        document.getElementById('paratele').innerHTML = 'just 10 chiffres.';
+    }
+    else {
+        document.getElementById('paratele').innerHTML = 'Telephone Non Valid.';
+        document.getElementById('telephone').style.borderColor="red";
+
+        
+    }
+
+
+}
+
+
+
+
+
+
+
 document.getElementById('btn').addEventListener('click',function(){
 
 
@@ -142,6 +261,6 @@ document.getElementById('btn').addEventListener('click',function(){
             document.getElementById("paraclb").innerHTML = "7adk 3 xd";
         
         }
-    } 
+    }
 
 });
